51b9e4ed2dc2cee8f42a3d2d3901ddd6d79118fd
Thinh Truong <steve@Thinhs-MacBook-Pro.local>
2021-02-06 09:17:00 +0700

    update data

--- README.md
+++ README.md
@@ -3,16 +3,17 @@
 Based on the code from https://github.com/monologg/JointBERT
 
 ## Model implemented:
-- JointBERT, JointMBERT, JointDistilBERT, JointAlBERT
-- JointRoBERTa, JointXLM-R, JointPhoBERT
+- JointBERT, JointDistilBERT, JointAlBERT
+- JointMBERT JointRoBERTa, JointXLM-R, JointPhoBERT
 
 ## Added Early Stopping
 
 ## Tuning metrics to choose:
 - Validation loss
 - Intent accuracy
-- Slot F-1
+- Slot F-1 (Main metrics)
 - Semantic Frame Accuracy (Both intent and slots need to be correct)
+
 
 ## Run
 
---
+++ run_bert_crf_attn.sh
@@ -0,0 +1,24 @@
+lr_list=(1e-5 2e-5 3e-5 4e-5 5e-5)
+for lr in "${lr_list[@]}" 
+do
+echo "${lr}"
+export MODEL_DIR=atis_bert_crf_attn_256
+export MODEL_DIR=$MODEL_DIR"/"$lr
+echo "${MODEL_DIR}"
+/usr/bin/python3.7 main.py --task atis \
+                  --model_type bert \
+                  --model_dir $MODEL_DIR \
+                  --data_dir data \
+                  --seed 1 \
+                  --do_train \
+                  --do_eval \
+                  --num_train_epochs 1000 \
+                  --use_crf \
+                  --save_steps 140 \
+                  --logging_steps 140 \
+                  --tuning_metric mean_intent_slot \
+                  --use_intent_context_attention \
+                  --intent_embedding_size 256 \
+                  --attention_embedding_size 256 \
+                  --learning_rate $lr
+done
